9aeaa4295fd6838c68be1a88781e176a31621901
Alexander <adgbq2@mail.missouri.edu>
2019-08-27 11:58:54 -0600

    styled a few components and set up scss file structure

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -75,6 +75,7 @@ export default class IndecisionApp extends React.Component {
        return (
            <div>
            <Header subtitle={subtitle}/>
+           <div className="container">
            <Action 
            hasOptions={this.state.options.length > 0} 
                handlePick={this.handlePick}
@@ -87,6 +88,7 @@ export default class IndecisionApp extends React.Component {
            <AddOption 
                handleAddOption={this.handleAddOption}
            />
+           </div>
            <OptionModal
            selectedOption={this.state.selectedOption} 
            handleModalClick={this.handleModalClick}
